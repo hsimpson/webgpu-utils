e1dfd2b1495cefa313e17864155338c34ae70794
Daniel Toplak <homer.simpson123@gmx.de>
2025-03-22 12:56:22 +0100

    fix: code formating

--- examples/triangle/triangle.ts
+++ examples/triangle/triangle.ts
@@ -1,12 +1,30 @@
-import { createContext } from '../../dist/index.mjs';
+import { WebGPUContext } from '../../dist/';
 
 async function initalization() {
   // get the canvas element
   const canvas = document.getElementById('webgpu_canvas') as HTMLCanvasElement;
 
   // create a webgpu context
-  const webGPUContext = await createContext(canvas);
-  console.log('WebGPU Context: ', webGPUContext);
+  const webGPUContext = new WebGPUContext(canvas);
+  const result = await webGPUContext.create();
+
+  if (!result) {
+    console.error('Failed to create WebGPU context');
+    return;
+  }
+
+  // size of canvas
+  const presentationSize = { width: canvas.width, height: canvas.height };
+
+  // configure the presenation context
+  webGPUContext.gpuCanvasContext.configure({
+    device: webGPUContext.device,
+    format: webGPUContext.preferredCanvasFormat,
+    alphaMode: 'opaque',
+  });
+
+  // create a render target
+  // const renderTarget = webGPUContext.
 }
 
 // the render loop
